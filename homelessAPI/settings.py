b0030f890bc144039ea1cba2616e75109e18cd95
pitonisaX <karreyes@gmail.com>
2017-03-09 19:28:44 -0800

    using same django staticpath as in assignment 9

--- homelessAPI/settings.py
+++ homelessAPI/settings.py
@@ -138,8 +138,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
-STATIC_ROOT = 'staticfiles'
-STATIC_URL = "/homeless/static/"
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR + STATIC_URL
+#STATIC_ROOT = 'staticfiles'
+#STATIC_URL = "/homeless/static/"
 
 #STATIC_ROOT = BASE_DIR + STATIC_URL
 #STATIC_ROOT = BASE_DIR + '/homelessApp/static/'
